18606fbf4fbc57c8a54a014cd6fbca48c55c79f8
TheGr8Chibi <43129699+TheGr8Chibi@users.noreply.github.com>
2018-09-10 10:43:35 +0200

    opdateret

--- main.c
+++ main.c
@@ -1,27 +1,45 @@
 #pragma config(Sensor, S1,     touch,          sensorEV3_Touch)
 #pragma config(Sensor, S2,     color,          sensorEV3_Color)
 
+//Line following
+void lineFollower(int target, float multiplier){
+	string t1;
+	int col1 = getColorReflected(S2);
+	sprintf(t1,"farve: %d", col1);
+	displayCenteredBigTextLine(1,t1);
+
+	int error = col1 - target;
+	float difSpeed = error * multiplier;
+
+	setMotorSync(motorA, motorB, difSpeed, 50);
+}
+
+//Stop motors
+void stopMotors(){
+	setMotorSync(motorA, motorB, 0, 0);
+}
+
+//Main program
 task main()
 {
-
-	int col1;
-	string t1;
-	int target = 40;
-	int error;
-	float multiplier = 0.7;
-	float targetSpeed;
-
 	while(true){
-		col1 = getColorReflected(S2);
-		sprintf(t1,"farve: %d", col1);
-		displayCenteredBigTextLine(1,t1);
+		//Section 1
+		while(getColorReflected(S2) > 15){
+			lineFollower(40, 0.7);
+		}
+		stopMotors();
 
-		error = col1 - target;
-		targetSpeed = error * multiplier;
+		//Drej
+		setMotorTarget(motorB,5, 30);
+		setMotorTarget(motorA,5, 30);
+		setMotorTarget(motorA,5, 30);
+		while(getColorReflected(S2) > 80){
+			setMotorSync(motorA, motorB, 0, 50);
+		}
+		stopMotors();
 
-		setMotorSync(motorA, motorB, targetSpeed, 50);
-
-
+		while(getColorReflected(S2) > 15){
+			lineFollower(40, 0.7);
+		}
 	}
-
 }
